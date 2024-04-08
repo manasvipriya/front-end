770ad84056c2862409bc4629e9aed149715a7c59
manasvipriya <priyamanasvi3@gmail.com>
2024-04-08 11:23:13 +0530

    updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -14,10 +14,10 @@ const Navbar = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={() => {setMenu("shop")}}>Shop<hr/></li>
-                <li onClick={() => {setMenu("men")}}>Men</li>
-                <li onClick={() => {setMenu("women")}}>Women</li>
-                <li onClick={() => {setMenu("kids")}}>Kids</li>
+                <li onClick={() => {setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
---
+++ src/pages/shop.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const shop = () => {
+  return (
+    <div>shop</div>
+  )
+}
